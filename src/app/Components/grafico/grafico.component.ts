91c471ccf7c00ae3c6ee650745c4efcca196410a
airsoftpescaymas <airsoftpescaymas@gmail.com>
2024-10-28 18:19:07 +0000

    new commit

--- src/app/Components/grafico/grafico.component.ts
+++ src/app/Components/grafico/grafico.component.ts
@@ -1,23 +1,39 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-grafico',
   standalone: true,
-  imports: [NgIf],
+  imports: [CommonModule],
   templateUrl: './grafico.component.html',
   styleUrls: ['./grafico.component.css']
 })
-export class GraficoComponent {
+export class GraficoComponent implements OnChanges {
+  @Input() pokemonId: number = 1;
+  nombre: string = '';
+  types: string[] = [];
+  stats = {
+    hp: 0,
+    atk: 0,
+    def: 0
+  };
 
-  // Atributos del Pokémon
-  nombre: string = 'Pikachu';
-  id: number = 25;
-  tipo: string = 'Messi';
-  ataques: number = 3;
-  ataque: number = 55;
-  defensa: number = 40;
-  vidaPokemon: number = 100;
+  ngOnChanges() {
+    this.actualizarStats();
+  }
 
- 
+  private actualizarStats() {
+    // Simulación de stats basados en el ID
+    this.stats.hp = 45 + (this.pokemonId % 100);
+    this.stats.atk = 40 + (this.pokemonId % 80);
+    this.stats.def = 35 + (this.pokemonId % 90);
+    
+    // Nombres de ejemplo (podrías conectar con PokeAPI para datos reales)
+    const nombres = ['Bulbasaur', 'Charmander', 'Squirtle', 'Pikachu'];
+    this.nombre = nombres[this.pokemonId % nombres.length];
+    
+    // Tipos de ejemplo
+    const tiposDisponibles = ['Fuego', 'Agua', 'Planta', 'Eléctrico'];
+    this.types = [tiposDisponibles[this.pokemonId % tiposDisponibles.length]];
+  }
 }
--- src/app/Components/pokedex-view/pokedex-view.component.ts
+++ src/app/Components/pokedex-view/pokedex-view.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PokedexComponent } from '../pokedex/pokedex.component';
 import { GraficoComponent } from '../grafico/grafico.component';
+import { CommonModule } from '@angular/common'; // Importa el módulo común
 
 @Component({
   selector: 'app-pokedex-view',
   standalone: true,
-  imports: [PokedexComponent, GraficoComponent],
+  imports: [CommonModule, PokedexComponent, GraficoComponent], // Asegúrate de incluir CommonModule
   templateUrl: './pokedex-view.component.html',
-  styleUrls: ['./pokedex-view.component.css']
+  styleUrls: ['./pokedex-view.component.css'],
 })
 export class PokedexViewComponent {
   pokemonId: number = 0;
@@ -17,5 +18,4 @@ export class PokedexViewComponent {
     this.pokemonId = this.pokemonId + event;
     this.pokemonImagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`;
   }
-
 }
